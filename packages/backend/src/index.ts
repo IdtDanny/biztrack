4798b0f50baa83a5317a4ce8526fb587c0a86d09
Danny_Idt <idtdanny@gmail.com>
2026-08-15 00:15:09 +0200

    V5 Expect

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -16,3 +16,22 @@ export default async function handler(req: any, res: any) {
   const app = await bootstrap();
   app.getHttpAdapter().getInstance()(req, res);
 }
+
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// let cachedServer: any;
+
+// async function bootstrap() {
+//   if (!cachedServer) {
+//     const app = await NestFactory.create(AppModule);
+//     await app.listen(3000);
+//     cachedServer = app;
+//   }
+//   return cachedServer;
+// }
+
+// export default async function handler(req: any, res: any) {
+//   const app = await bootstrap();
+//   app.getHttpAdapter().getInstance()(req, res);
+// }
