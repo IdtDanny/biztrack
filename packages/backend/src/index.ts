3d178087a4bdec45b135df49b1ade333cad6ed23
Danny_Idt <idtdanny@gmail.com>
2026-08-15 00:42:48 +0200

    V9 Expect

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,20 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
 
 let cachedServer: any;
 
 async function bootstrap() {
   if (!cachedServer) {
-    const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
-    cachedServer = app;
+    const expressApp = express();
+    const app = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(expressApp),
+    );
+    await app.init();
+    cachedServer = expressApp;
   }
   return cachedServer;
 }
 
 export default async function handler(req: any, res: any) {
-  const app = await bootstrap();
-  app.getHttpAdapter().getInstance()(req, res);
+  const server = await bootstrap();
+  server(req, res);
 }
 
 // import { NestFactory } from '@nestjs/core';
