4a417cfd1f291d146809235e3b314c862b97de6f
Danny_Idt <idtdanny@gmail.com>
2026-08-15 10:21:58 +0200

    V19 Patch

--- packages/backend/src/prisma/prisma.service.ts
+++ packages/backend/src/prisma/prisma.service.ts
@@ -1,12 +1,12 @@
-import 'dotenv/config';
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  constructor() {
-    const databaseUrl = process.env.DATABASE_URL;
+  constructor(private configService: ConfigService) {
+    const databaseUrl = configService.get<string>('DATABASE_URL');
     if (!databaseUrl) {
       throw new Error('DATABASE_URL is not defined');
     }
